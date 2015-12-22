cc992e5366af83ced8d0ee4a442d45124c8f2d10
atmarksharp <atmarksharp@live.jp>
2015-12-22 10:00:57 +0900

    Updated sql and add proto db

--- csv2sql.rb
+++ csv2sql.rb
@@ -8,14 +8,14 @@ if ARGV[0] == nil
 end
 
 puts "CREATE TABLE `words` ("
-puts "  id integer primary key autoincrement,"
-puts "  sub_id integer,"
-puts "  word text,"
-puts "  mean text,"
-puts "  level integer"
+puts "  `id` integer primary key autoincrement,"
+puts "  `sub_id` integer,"
+puts "  `word` text,"
+puts "  `mean` text,"
+puts "  `level` integer"
 puts ");"
 puts ""
 
 CSV.foreach(ARGV[0]) do |row|
-  puts "INSERT INTO `words` (id, sub_id, word, mean, level) values (#{row[0]}, #{row[1]}, \"#{row[2]}\", \"#{row[3]}\", #{row[4]});"
+  puts "INSERT INTO `words` (`id`, `sub_id`, `word`, `mean`, `level`) values (#{row[0]}, #{row[1]}, \"#{row[2]}\", \"#{row[3]}\", #{row[4]});"
 end
