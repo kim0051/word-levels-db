c7342fd70d819f8af66c594428bd11c7e3b80553
atmarksharp <atmarksharp@live.jp>
2015-12-22 10:27:23 +0900

    Fixed parse error of sql and script

--- csv2sql.rb
+++ csv2sql.rb
@@ -8,14 +8,14 @@ if ARGV[0] == nil
 end
 
 puts "CREATE TABLE `words` ("
-puts "  `id` integer primary key autoincrement,"
-puts "  `sub_id` integer,"
-puts "  `word` text,"
-puts "  `mean` text,"
-puts "  `level` integer"
+puts "  `id` INTEGER PRIMARY KEY AUTOINCREMENT,"
+puts "  `groupid` INTEGER,"
+puts "  `word` TEXT UNIQUE,"
+puts "  `mean` TEXT,"
+puts "  `level` INTEGER"
 puts ");"
 puts ""
 
 CSV.foreach(ARGV[0]) do |row|
-  puts "INSERT INTO `words` (`id`, `sub_id`, `word`, `mean`, `level`) values (#{row[0]}, #{row[1]}, \"#{row[2]}\", \"#{row[3]}\", #{row[4]});"
+  puts "INSERT INTO `words` (`id`, `groupid`, `word`, `mean`, `level`) values (#{row[0]}, #{row[1]}, \"#{row[2]}\", \"#{row[3]}\", #{row[4]});"
 end
